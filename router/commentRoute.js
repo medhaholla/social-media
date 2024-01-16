732dea9ed4a874defae29e5647af1e99d3e28db8
medhaholla <holla.medha@gmail.com>
2024-01-16 22:02:55 +0530

    Implement route to retrieve comments for a specific post

--- router/commentRoute.js
+++ router/commentRoute.js
@@ -24,4 +24,15 @@ router.post("/:postId", async (req, res) => {
   }
 });
 
+router.get("/:postId", async (req, res) => {
+  const postId = req.params.postId;
+
+  try {
+    const comments = await Comment.find({ post: postId });
+    res.json(comments);
+  } catch (err) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 module.exports = router;
